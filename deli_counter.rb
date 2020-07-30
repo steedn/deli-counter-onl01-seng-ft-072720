e93a43563f99c6f09a50c2741504efcfca9b88fa
Nathan Steed <steedn@outlook.com>
2020-07-30 03:14:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ require "pry"
 katz_deli = []
 def line(katz_deli)
   if katz_deli == []
-    puts "The line is currently empty"
+    puts "The line is currently empty."
   else
     puts katz_deli
   end
