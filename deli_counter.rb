5679919268907b5afeed90eb00c4b78ade573d6a
Nathan Steed <steedn@outlook.com>
2020-07-30 03:40:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,7 @@
 require "pry"
 
 katz_deli = []
-def line(katz_deli)
-  if katz_deli == []
-    puts "The line is currently empty."
-  else
-    puts "The line is currently: #{array}"
-  end
-end
+
 def take_a_number(array)
   counter = 1
   array = katz_deli
@@ -15,8 +9,17 @@ def take_a_number(array)
     array[counter].inject(0 , counter + ".")
     counter += 1
   end
-  array
+array
   binding.pry
+end
+
+
+def line(katz_deli)
+  if katz_deli == []
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: #{array}"
+  end
 end
 
 # have "take a number method" add a '.' at the end of num, then when printing out katz_deli, .join it. .shift to take off
