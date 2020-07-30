3578fe13f9d33d29ba6944adb496f957655095ee
Nathan Steed <steedn@outlook.com>
2020-07-30 02:53:29 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
-# Write your code here.
+require "pry"# Write your code here.
+katz_deli = []
+def take_a_number(num, name)
+  array =  []
+  array << num
+  array << name
+  array << array.joining
+  binding.pry
+end
