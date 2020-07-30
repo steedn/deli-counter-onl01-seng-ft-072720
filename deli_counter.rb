4a9d1f6f36750e51c99bd4ee6a260a076b9a59a2
Nathan Steed <steedn@outlook.com>
2020-07-30 04:19:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,11 +22,11 @@ def line(array)
       puts "The line is currently empty."
     else
     array.each_with_index do |name, index|
-      new_array.push("#{index + 1}. #{name}")
+      new_array.push(" #{index + 1}. #{name}")
     end
       new_array.unshift("The line is currently: ")
       puts new_array.join
-      binding.pry
+      # binding.pry
   end
 end
 
