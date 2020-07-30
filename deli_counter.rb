84b231fa008f96b0e684dcb9b3d7b69941c2e101
Nathan Steed <steedn@outlook.com>
2020-07-30 03:11:58 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,19 @@
 require "pry"
 
 katz_deli = []
+def line
+  if katz_deli == 0
+    puts "The line is currently empty"
+  else
+    puts katz_deli
+  end
 def take_a_number(num, name)
   array =  []
-  array << num
+  array_num = []
   array << name
-  array << array.joining
-  binding.pry
+  array_num << num
+
+  # binding.pry
 end
+
+# have "take a number method" add a '.' at the end of num, then when printing out katz_deli, .join it. .shift to take off  
