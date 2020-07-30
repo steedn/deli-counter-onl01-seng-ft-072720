ee10f6226d6722e41bed713fab8256ca7db9ef11
Nathan Steed <steedn@outlook.com>
2020-07-30 03:33:28 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,13 +8,15 @@ def line(katz_deli)
     puts katz_deli
   end
 end
-def take_a_number(num, name)
-  array =  []
-  array_num = []
-  array << name
-  array_num << num
-
-  # binding.pry
+def take_a_number(array)
+  counter = 1
+  array = katz_deli
+  array.each do |name|
+    array[counter].inject(0 , counter)
+    counter += 1
+  end
+  array
+  binding.pry
 end
 
 # have "take a number method" add a '.' at the end of num, then when printing out katz_deli, .join it. .shift to take off
